3199907af9b8b7b1422b091d587a54f14c6f4707
riccardo <riccardo.vannozzi@live.com>
2019-08-18 18:10:09 +0200

    Aggiunto punto e virgola in main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // Modules
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow} = require('electron');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 // Create a new BrowserWindow when `app` is ready
 function createWindow () {
@@ -14,7 +14,7 @@ function createWindow () {
   })
 
   // Load index.html into the new BrowserWindow
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('index.html');
 
   // Open DevTools - Remove for PRODUCTION!
   mainWindow.webContents.openDevTools();
@@ -28,20 +28,20 @@ function createWindow () {
 // Electron `app` is ready
 app.on('ready', () => {
 
-  console.log(app.getPath('desktop'))
-  console.log(app.getPath('music'))
-  console.log(app.getPath('temp'))
-  console.log(app.getPath('userData'))
+  console.log(app.getPath('desktop'));
+  console.log(app.getPath('music'));
+  console.log(app.getPath('temp'));
+  console.log(app.getPath('userData'));
 
   createWindow()
-})
+});
 
 // Quit when all windows are closed - (Not macOS - Darwin)
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
-})
+});
 
 // When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
 app.on('activate', () => {
   if (mainWindow === null) createWindow()
-})
+});
